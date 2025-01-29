eb4257f2aefbf2deca2a7c99428aff96aaa57197
XANIY <anirudhrk11@gmail.com>
2025-01-29 09:31:20 +0530

    Update and rename README.md to welcome.java

---
+++ welcome.java
@@ -0,0 +1,5 @@
+class welc
+  { public static boid main (String args[]
+                             {  System.out.println("WELCOME TO ALL THE CHAI PEOPLE GUYS");
+                             }
+  }

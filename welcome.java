90a3774d04befc91754c805446a87528c9a5e4dc
XANIY <anirudhrk11@gmail.com>
2025-01-29 09:32:08 +0530

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -1,5 +1,5 @@
 class welc
-  { public static boid main (String args[]
+  { public static void main (String args[]
                              {  System.out.println("WELCOME TO ALL THE CHAI PEOPLE GUYS");
                              }
   }
